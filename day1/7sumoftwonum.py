7cfc461f46ba033561901c6f46f9d575a681ac69
learner0018 <212705726+learner0018@users.noreply.github.com>
2026-06-19 15:36:01 +0530

    adding the program for sum of two numbers

--- day1/7sumoftwonum.py
+++ day1/7sumoftwonum.py
@@ -16,6 +16,7 @@ def add(a, b):
 
 def main():
     num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
     total = add(num1, num2)
     print(f"The sum of {num1} and {num2} is: {total}")
 
