f4010c537e1033b89b3631cf28de7d97b243e4aa
learner0018 <212705726+learner0018@users.noreply.github.com>
2026-06-19 15:13:32 +0530

    adding multiplication table program

---
+++ day1/5multiplicationtable.py
@@ -0,0 +1,22 @@
+#algorithm
+"""
+start 
+prompt the user to enter a number
+read the input and store it as a numeric value(integer or float)
+for i from 1 to 10 (inclusive)
+-compute product = number *1
+-print the equation in the format : number *i = product
+END"""
+
+
+def print_multiplication_table():
+    fir i in range(1, 11):
+        print(f"{n} * {i} = {n*i}")
+    
+
+def main():
+    n = float(input("Enter a number: "))
+    print_multiplication_table(n)
+
+if __name__ == "__main__":
+    main()
